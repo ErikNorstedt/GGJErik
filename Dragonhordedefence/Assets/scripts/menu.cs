5c3911e291ad8def6aa38855b9abe35edfd1bcb1
Fourzifer <30759328+Fourzifer@users.noreply.github.com>
2019-01-27 12:02:59 +0100

    Diverse menyer

---
+++ Dragonhordedefence/Assets/scripts/menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+
+    //change index in final version
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+
+    //change index in final version
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
